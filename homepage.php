7afe70db8960dd26c8a3fdc5f12682f53ce8691e
Al-Nahian <nazme.nahian@northsouth.edu>
2023-08-18 23:28:42 +0600

    v 1.0.14

--- homepage.php
+++ homepage.php
@@ -26,8 +26,8 @@
 
 <div >
 <div class=" h-40 w-40 px">
-  <div class="absolute top-12 left-10 py-5">
-    <img class="rounded-full h-28 w-28 " src="https://img.freepik.com/free-photo/indoor-picture-cheerful-handsome-young-man-having-folded-hands-looking-directly-smiling-sincerely-wearing-casual-clothes_176532-10257.jpg?w=1380&t=st=1692119371~exp=1692119971~hmac=45fb3f9f754fdfa36e846d07d46baeb4c7a9fff3520826b8e31dc512e4a67baa" alt="">
+  <div class="absolute top-12 left-9 py-5">
+    <img class="rounded-full h-28 w-28 " src="img/profile.png" alt="">
   </div>
 </div>
 
@@ -48,9 +48,9 @@
   <div class="max-w-[1070px] mx-auto grid lg:grid-cols-4 md:grid-cols-2 gap-28">
 
 <a href="https://www.facebook.com">
-    <div class="text-center shadow-lg bg-blue-200 rounded-2xl">
+    <div class="text-center shadow-lg bg-blue-200 rounded-3xl">
       <div class="overflow-hidden">
-      <img src="img/inventory.png" class=" hover:scale-125 duration-1000"/>
+      <img src="img/inventory.png" class=" hover:scale-125 duration-500"/>
       </div>
       <div class="font-bold text-xl mb-2">Inventory</div>
     </div> 
@@ -58,26 +58,26 @@
 
           <a href="https://www.facebook.com">
 
-      <div class="text-center shadow-lg bg-blue-200 rounded-2xl">
+      <div class="text-center shadow-lg bg-gray-200 rounded-3xl">
       <div class="overflow-hidden rounded-2xl">
-      <img src="img/daily-report.png" class="hover:scale-125 duration-1000"/>
+      <img src="img/daily-report.png" class="hover:scale-125 duration-500"/>
       </div>
       <div class="font-bold text-xl mb-2">Daily Report</div>
     </div></a>
 
        <a href="https://www.facebook.com">
-        <div class="text-center shadow-lg bg-blue-200 rounded-2xl">
+        <div class="text-center shadow-lg bg-orange-200 rounded-3xl">
       <div class="overflow-hidden">
-      <img src="img/businessman.png" class="hover:scale-125 duration-1000"/>
+      <img src="img/businessman.png" class="hover:scale-125 duration-500"/>
       </div>
       <div class="font-bold text-xl mb-2">DSR</div>
     </div></a>
 
 
         <a href="https://www.facebook.com">
-        <div class="text-center shadow-lg bg-blue-200 rounded-2xl">
+        <div class="text-center shadow-lg bg-red-200 rounded-3xl">
         <div class="overflow-hidden">
-        <img src="img/order.png" class="hover:scale-125 duration-1000"/>
+        <img src="img/order.png" class="hover:scale-125 duration-500"/>
         </div>
         <div class="font-bold text-xl mb-2">Orders</div>
          </div>
--- logout.php
+++ logout.php
@@ -3,7 +3,5 @@
     unset($_SESSION['username']);
     unset($_SESSION['firstname']);
     unset($_SESSION['loggedin']);
-    setcookie('number', $number, time() - (60*60*24*365));
-    setcookie("password", $password, time() - 60*60*24*365);
     header("location:login.php");
 ?>
